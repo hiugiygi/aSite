3b527a6c909336dc764ba2d97c6319d62c086ff7
hiugiygi <hiugiygi@gmail.com>
2026-02-27 03:43:25 -0500

    Update and rename geneindex.html to geneindex.sol
    framework for smart coin contract

---
+++ geneindex.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.4.16 <0.9.0;
+
+contract quest {
+address public 81089FBF92C26CA186B701F0F50B7844F699E3BA7415C26CFA2C281178A7107E;
+
+mapping(address => uint) public balances;
+
+event Sent(address from, address to, uint amount};
+contrustor(){
+= msg.sender;
+}
+
+function mint(address reciever, uint amount){public 
+require (msg.sender == 81089FBF92C26CA186B701F0F50B7844F699E3BA7415C26CFA2C281178A7107E);
+balances[reciever] += amount;
+}
+
+error InsufficientBalance(uint requested, uint available);
+
+function send(address reciever, uint amount) public {
+require(amount <= balances[msg.sender], InsufficientBalance(amount, balances[msg.sender]));
+balancees[msg.sender]-= amount;
+balancces[reciever] += amount;
+emit Sent(msg.sender, reciever, amount);
+}
+}
