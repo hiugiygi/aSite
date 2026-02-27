373d5f111830f3dfdeac61910c5a0135499ba741
hiugiygi <hiugiygi@gmail.com>
2026-02-27 03:54:45 -0500

    Update geneindex.sol
    typos

--- geneindex.sol
+++ geneindex.sol
@@ -6,7 +6,7 @@ address public 81089FBF92C26CA186B701F0F50B7844F699E3BA7415C26CFA2C281178A7107E;
 mapping(address => uint) public balances;
 
 event Sent(address from, address to, uint amount};
-contrustor(){
+contructor(){
 = msg.sender;
 }
 
@@ -19,8 +19,8 @@ error InsufficientBalance(uint requested, uint available);
 
 function send(address reciever, uint amount) public {
 require(amount <= balances[msg.sender], InsufficientBalance(amount, balances[msg.sender]));
-balancees[msg.sender]-= amount;
-balancces[reciever] += amount;
+balances[msg.sender]-= amount;
+balances[reciever] += amount;
 emit Sent(msg.sender, reciever, amount);
 }
 }
